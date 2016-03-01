5beda6e16bac4b4fbb9f1f0e1aa1139e60f7a43c
Dexter Gramfors <dexter.gramfors@trioptima.com>
2016-03-01 14:38:06 +0100

    Minor python code example fix.

--- code_examples/pipe_example.py
+++ code_examples/pipe_example.py
@@ -2,8 +2,8 @@ def worker(conn):
     conn.send("data")
     conn.close()
 
-parent_conn, child_conn = multiprocessing.Pipe()
-p = multiprocessing.Process(target=worker, args=(child_conn,))
+parent_conn, child_conn = Pipe()
+p = Process(target=worker, args=(child_conn,))
 p.start()
 handle_data(parent_conn.recv())
 p.join()
